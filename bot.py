3ca64cbae7327d9022ef79a342136bcbe70e4d2c
ntamares <ntamares@gmail.com>
2026-06-23 22:40:30 -0500

    make bot download video and reply to user with uploaded video

--- bot.py
+++ bot.py
@@ -1,23 +1,25 @@
-import os
+import asyncio
 import discord
+import os
 from dotenv import load_dotenv
 from discord.ext import commands
-from utils.link_parser import contains_supported_link
+from services.downloader import download_video
+from utils.link_parser import extract_supported_url
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-CHANNEL_ID_RAW = os.getenv("BOT_TESTING_CHANNEL_ID")
+# CHANNEL_ID_RAW = os.getenv("BOT_TESTING_CHANNEL_ID")
 
 if not TOKEN:
     raise ValueError("DISCORD_TOKEN environment variable is not set")
 
-if not CHANNEL_ID_RAW:
-    raise ValueError("BOT_TESTING_CHANNEL_ID environment variable is not set")
+# if not CHANNEL_ID_RAW:
+#     raise ValueError("BOT_TESTING_CHANNEL_ID environment variable is not set")
 
-try:
-    channel_id = int(CHANNEL_ID_RAW)
-except ValueError:
-    raise ValueError("BOT_TESTING_CHANNEL_ID must be an integer")
+# try:
+#     CHANNEL_ID = int(CHANNEL_ID_RAW)
+# except ValueError:
+#     raise ValueError("BOT_TESTING_CHANNEL_ID must be an integer")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -37,14 +39,26 @@ async def on_message(message):
     if message.author.bot:
         return
     
-    # TODO remove once done testing
-    if message.channel.id == channel_id:
-        if contains_supported_link(message.content):
-            print("Supported link detected")
-            await message.channel.send("Supported link was detected")
-        else:
-            print("Domain not supported")
-            await message.channel.send("Domain not supported")
+    url = extract_supported_url(message.content)
+
+    #TODO better/more accurate logging
+
+    if extract_supported_url(message.content) and url:        
+        try:
+            print("Downloading video")
+            file_path = await asyncio.to_thread(download_video, url)
+
+            await message.reply(
+                file=discord.File(file_path),
+                mention_author=False
+            )
+
+            os.remove(file_path)
+
+        except Exception as e:
+            await message.channel.send(f"Download failed: {e}")
+    else:
+        print("Domain not supported")
 
     await bot.process_commands(message)
 
